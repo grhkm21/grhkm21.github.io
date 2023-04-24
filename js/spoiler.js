7c0e4fe3a0dafc20bdb19bb98d35aa57bebad3b3
Gareth Ma <grhkm21@gmail.com>
2023-04-24 09:45:43 +0100

    rebuilding site Mon 24 Apr 2023 09:45:43 BST

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,11 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var switchesSelector = ".spoiler_block_show,.spoiler_block_hide";
+    var contentSelector = ".spoiler_block_content";
 
-  var switchesSelector = '.spoiler_block_show,.spoiler_block_hide';
-  var contentSelector = '.spoiler_block_content';
-
-  $(document).on('click', switchesSelector, function() {
-    var selector = switchesSelector + ',' + contentSelector;
-    $(this).parent().children(selector).toggle();
-    return false;
-  });
+    $(document).on("click", switchesSelector, function () {
+        var selector = switchesSelector + "," + contentSelector;
+        $(this).parent().children(selector).toggle();
+        return false;
+    });
 });
